14ca75eec27f91db9b68e03dadd24862047d1305
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2021-08-26 04:20:38 +0200

    normal dist

--- server.R
+++ server.R
@@ -1,3 +1,7 @@
 server <- function(input, output, session) {
-  
+ output$normal_plot <- renderPlot({
+   ggplot(data = data.frame(x = c(-5, 5)), aes(x = x)) + 
+     stat_function(geom = "area", fun = ifelse(input$normal_type == "Sűrűség-függvény", "dnorm", "pnorm"), args = list(input$normal_mean, input$normal_sd), alpha = .3, color = "black") + 
+     scale_y_continuous(limits = c(0, ifelse(input$normal_type == "Sűrűség-függvény", .6, .9)), expand = c(0, 0))
+ }) 
 }
--- ui.R
+++ ui.R
@@ -3,8 +3,23 @@ ui <- dashboardPage(
   dashboardHeader(title = "Statisztika II."),
   dashboardSidebar(
     sidebarMenu(
-      menuItem("Dashboard", tabName = "dashboard", icon = icon("quetion")),
-      menuItem("Nevezetes eloszlások", icon = icon("chart"), tabName = "distribution")
+      menuItem("Mi ez a projekt?", tabName = "what", icon = icon("question")),
+      menuItem("Nevezetes eloszlások", icon = icon("area-chart"), 
+               menuSubItem("Normális", tabName = "normal")
+               )
   )),
-  dashboardBody()
+  dashboardBody(
+    tabItems(
+      tabItem(tabName = "normal",
+              box(width = 8,
+                  sliderInput(inputId = "normal_mean", label = "Várható érték", value = 0, min = -5, max = 5, step = .2),
+                  sliderInput(inputId = "normal_sd", label = "Szórás", value = 1, min = 0, max = 5, step = .1),
+                  selectInput(inputId = "normal_type", label = "Függvény:", 
+                              choices = c("Eloszlás-függvény", "Sűrűség-függvény")
+                              ),
+                  plotOutput("normal_plot")
+                  )
+              )
+    )
+  )
 )
