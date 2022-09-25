5e268878b045970f8ee7b56ac3e7e57ab7173cdb
Marcell Granát <64445649+MarcellGranat@users.noreply.github.com>
2022-09-25 17:35:20 +0200

    error handling

--- global.R
+++ global.R
@@ -1,5 +1,22 @@
-library(shiny)
-library(shinydashboard)
-library(tidyverse)
+package_success <- FALSE
+tryCatch(expr = {
+  library(shiny)
+  library(shinydashboard)
+  library(shinyWidgets)
+  library(tidyverse)
+  package_success <- TRUE
+}, error = function(e) {message(e)}
+)
 
+if (!package_success) {
+  install.packages("shiny")
+  install.packages("shinydashboard")
+  install.packages("shinyWidgets")
+  install.packages("tidyverse")
+  
+  library(shiny)
+  library(shinydashboard)
+  library(shinyWidgets)
+  library(tidyverse)
+}
 
